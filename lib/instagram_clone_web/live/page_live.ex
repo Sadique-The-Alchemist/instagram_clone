d545242026faa1306a04f57898dc312155b70ef4
Sadique <sadique.pal@gmail.com>
2021-08-01 22:16:04 +0530

    new form

--- lib/instagram_clone_web/live/page_live.ex
+++ lib/instagram_clone_web/live/page_live.ex
@@ -9,4 +9,19 @@ defmodule InstagramCloneWeb.PageLive do
     changeset = Accounts.change_user_registration(%User{})
     {:ok, socket |> assign(changeset: changeset) |> assign(trigger_submit: false)}
   end
+
+  @impl true
+  def handle_event("validate", %{"user" => user_params}, socket) do
+    changeset =
+      %User{}
+      |> User.registration_changeset(user_params)
+      |> Map.put(:action, :validate)
+
+    :timer.sleep(9000)
+    {:noreply, socket |> assign(changeset: changeset)}
+  end
+
+  def handle_event("save", _, socket) do
+    {:noreply, assign(socket, trigger_submit: true)}
+  end
 end
